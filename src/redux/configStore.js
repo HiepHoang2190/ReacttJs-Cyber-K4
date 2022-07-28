ebd8eacb51aec74a6b8ec7e30ccaec6434799478
hieplotus <hieph@lotusoutsourcing.com>
2022-07-28 17:24:15 +0700

    video14_todolist_redux_api_setup_p1

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,16 +1,17 @@
 import {combineReducers, createStore} from 'redux';
+import ToDoListReducer from './reducers/ToDoListReducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-
-
-
+const composedEnhancers = composeWithDevTools()
 
 const rootReducer = combineReducers({
     //reducer khai báo tại đây
+    ToDoListReducer
 })
 
 
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,composedEnhancers);
 
 
 
