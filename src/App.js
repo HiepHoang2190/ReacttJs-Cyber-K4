08636fa444173e39e8efb130928d4084c2dd8174
Hiep Hoang <hieph@lotusoutsourcing.com>
2022-07-31 21:30:43 +0700

    video29 Jira TemplateLogin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Outlet, Route, Routes } from 'react-router-dom';
 import Header from './components/Home/Header/Header';
 import About from './pages/About/About'
 import Contact from './pages/Contact/Contact'
@@ -9,29 +9,16 @@ import Todolist from './pages/Todolist/Todolist';
 import ToDoListRedux from './pages/Todolist/ToDoListRedux';
 import ToDoListReduxSaga from './pages/BaiTapToDoListSaga/BaiTapToDoListSaga';
 import LoadingComponent from './components/GlobalSetting/LoadingComponent/LoadingComponent'
-import DemoHOCModal from './pages/DemoHOCModal/DemoHOCModal';
-import Modal from './HOC/Modal/Modal';
-
+import { UserLoginTemplate } from './templates/HomeTemplate/UserLoginTemplate';
 function App() {
   return (
     <div>
     
-      <Router>
+   
       <Header />
-      <Modal/>
       <LoadingComponent/>
-      <Routes>
-        <Route path='/home' element={<Home />} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/' element={<Home/>} />
-        <Route path='/todolistrcc' element={<Todolist/>} />
-        <Route path='/todolistrfc' element={<TodolistRFC/>} />
-        <Route path='/todolistredux' element={<ToDoListRedux/>} />
-        <Route path='/todolistreduxsaga' element={<ToDoListReduxSaga/>} />
-        <Route path='/demohocmodal' element={<DemoHOCModal/>} />
-      </Routes>
-  </Router>
+   <Outlet/>
+
     </div>
 
 
--- src/index.js
+++ src/index.js
@@ -3,17 +3,42 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './pages/About/About'
+import Contact from './pages/Contact/Contact'
+import Home from './pages/Home/Home'
+import TodolistRFC from './pages/Todolist/TodolistRFC';
+import Todolist from './pages/Todolist/Todolist';
+import ToDoListRedux from './pages/Todolist/ToDoListRedux';
+import ToDoListReduxSaga from './pages/BaiTapToDoListSaga/BaiTapToDoListSaga';
+import DemoHOCModal from './pages/DemoHOCModal/DemoHOCModal'
+import LoginCyberBugs from './pages/CyberBugs/LoginCyberBugs/LoginCyberBugs'
 
 //setup redux
 import store from './redux/configStore'
 import { Provider } from 'react-redux';
-
-import { BrowserRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-  
-      <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path='/home' element={<Home />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/todolistrcc' element={<Todolist />} />
+          <Route path='/todolistrfc' element={<TodolistRFC />} />
+          <Route path='/todolistredux' element={<ToDoListRedux />} />
+          <Route path='/todolistreduxsaga' element={<ToDoListReduxSaga />} />
+          <Route path='/demohocmodal' element={<DemoHOCModal />} />
+        </Route>
+        <Route path='/login' element={<LoginCyberBugs />} />
+      </Routes>
+    </Router>
 
 
   </Provider>
