ed146c1ce3cd8745ea6f55ae17250f12ca1a2bab
Hiep Hoang <hieph@lotusoutsourcing.com>
2022-08-06 16:46:22 +0700

    video38 createproject with authorization by token

--- src/services/CyberbugsService.js
+++ src/services/CyberbugsService.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import { DOMAIN_CYBERBUG } from "../util/constants/settingSystem"
+import { DOMAIN_CYBERBUG, TOKEN } from "../util/constants/settingSystem"
 
 export const cyberbugsService = {
   signinCyberBugs: (userLogin) => {
@@ -20,6 +20,15 @@ export const cyberbugsService = {
       url: `${DOMAIN_CYBERBUG}/Project/createProject`,
       method: 'POST',
       data: newProject
+    })
+  },
+  createProjectAuthorization: (newProject) => {
+    console.log(localStorage.getItem(TOKEN))
+    return Axios({
+      url: `${DOMAIN_CYBERBUG}/Project/createProjectAuthorize`,
+      method: 'POST',
+      data: newProject,
+      headers: { 'Authorization': 'Bearer ' + localStorage.getItem(TOKEN) } //JWT 
     })
   }
 }
